b198574aa736edfede8ddb911132b46473bbd917
theod <theo.delahogue@gmea.net>
2015-05-13 18:44:47 +0200

    Setting BUILD_WITH_INSTALL_RPATH to TRUE in order to change the install_name at linking

--- Shared/CMake/PdExternalCommon.cmake
+++ Shared/CMake/PdExternalCommon.cmake
@@ -57,15 +57,17 @@ elseif( NOT WIN32 )
 endif()
 
 if("${PROJECT_NAME}" STREQUAL "pd_Jamoma")
-        SET_TARGET_PROPERTIES(${PROJECT_NAME} PROPERTIES INSTALL_RPATH "${PD_LOADER_RPATH}")
-        install(TARGETS ${PROJECT_NAME}
-                        DESTINATION "${JAMOMAPD_INSTALL_FOLDER}/Jamoma"
-                        COMPONENT JamomaPd)
+    # use the install path when linking
+    SET_TARGET_PROPERTIES(${PROJECT_NAME} PROPERTIES BUILD_WITH_INSTALL_RPATH TRUE INSTALL_RPATH "${PD_LOADER_RPATH}")
+	install(TARGETS ${PROJECT_NAME}
+            DESTINATION "${JAMOMAPD_INSTALL_FOLDER}/Jamoma"
+            COMPONENT JamomaPd)
 else()
-        SET_TARGET_PROPERTIES(${PROJECT_NAME} PROPERTIES INSTALL_RPATH "${PD_EXTERNAL_RPATH}")
-        install(TARGETS ${PROJECT_NAME}
-                        DESTINATION "${JAMOMAPD_INSTALL_FOLDER}/Jamoma/externals"
-                        COMPONENT JamomaPd)
+    # use the install path when linking
+    SET_TARGET_PROPERTIES(${PROJECT_NAME} PROPERTIES BUILD_WITH_INSTALL_RPATH TRUE INSTALL_RPATH "${PD_EXTERNAL_RPATH}")
+	install(TARGETS ${PROJECT_NAME}
+            DESTINATION "${JAMOMAPD_INSTALL_FOLDER}/Jamoma/externals"
+            COMPONENT JamomaPd)
 endif()
 
 

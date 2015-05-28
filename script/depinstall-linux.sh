8e4bd2d4ff2e2b4a71ce2d8b25c91b31adbed35a
Antoine Villeret <antoine.villeret@gmail.com>
2015-05-28 22:19:47 +0200

    default to x86_64

--- script/depinstall-linux.sh
+++ script/depinstall-linux.sh
@@ -2,8 +2,13 @@
 
 case "$TRAVIS_OS_NAME" in
     linux)
-		wget http://www.cmake.org/files/v3.2/cmake-3.2.2-Linux-${ARCH}.tar.gz
-		sudo tar -xf cmake-3.2.2-Linux-${ARCH}.tar.gz -C /usr/local --strip-components=1
+		if [ ${ARCH} == "i386" ]
+			wget http://www.cmake.org/files/v3.2/cmake-3.2.2-Linux-i386.tar.gz
+			tar -xzf cmake-3.2.2-Linux-i386.tar.gz
+		else
+			wget http://www.cmake.org/files/v3.2/cmake-3.2.2-Linux-x86_64.tar.gz
+			tar -xzf cmake-3.2.2-Linux-x86_64.tar.gz
+		fi
 	;;
 	osx)
 		wget http://www.cmake.org/files/v3.2/cmake-3.2.2-Darwin-x86_64.tar.gz

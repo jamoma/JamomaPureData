5308f4ee657da165830631db2247b0ee591dd1d3
Antoine Villeret <antoine.villeret@gmail.com>
2015-05-28 13:52:12 +0200

    add case for OSX / Linux

--- script/depinstall-linux.sh
+++ script/depinstall-linux.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 
-wget http://www.cmake.org/files/v3.2/cmake-3.2.2-Linux-${ARCH}.tar.gz
-sudo tar -xf cmake-3.2.2-Linux-${ARCH}.tar.gz -C /usr/local --strip-components=1
+case "$TRAVIS_OS_NAME" in
+    linux)
+		wget http://www.cmake.org/files/v3.2/cmake-3.2.2-Linux-${ARCH}.tar.gz
+		sudo tar -xf cmake-3.2.2-Linux-${ARCH}.tar.gz -C /usr/local --strip-components=1
+	;;
+	osx)
+		wget http://www.cmake.org/files/v3.2/cmake-3.2.2-Darwin-x86_64.tar.gz
+		sudo tar -xf cmake-3.2.2-Darwin-x86_64.tar.gz -C /usr/local --strip-components=1
+	;;
+esac

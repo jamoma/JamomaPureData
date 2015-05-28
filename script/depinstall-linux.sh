729513be1e0ddcdd340680627f1661a4f15aefe7
Antoine Villeret <antoine.villeret@gmail.com>
2015-05-28 13:37:58 +0200

    make tar less verbose

--- script/depinstall-linux.sh
+++ script/depinstall-linux.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 wget http://www.cmake.org/files/v3.2/cmake-3.2.2-Linux-x86_64.tar.gz
-sudo tar -xvf cmake-3.2.2-Linux-x86_64.tar.gz -C /usr/local --strip-components=1
+sudo tar -xf cmake-3.2.2-Linux-x86_64.tar.gz -C /usr/local --strip-components=1

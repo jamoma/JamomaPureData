00af7e5d72a46b890459f6f091a30cd1bb5cc900
Antoine Villeret <antoine.villeret@gmail.com>
2015-05-29 16:15:15 +0200

    disable 32bit build for now

--- script/depinstall.sh
+++ script/depinstall.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
+mkdir -p /tmp/cmake
 case "$TRAVIS_OS_NAME" in
     linux)
 		sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 		sudo apt-get -qq update
 		sudo apt-get -qq install gcc-4.9 g++-4.9
-		mkdir -p /tmp/cmake
-		if [ "x${ARCH}" = "xi386" ]; then
+		if [ "x$(uname -m)" = "xi386" ]; then
 			wget http://www.cmake.org/files/v3.2/cmake-3.2.2-Linux-i386.tar.gz
 			tar -xzf cmake-3.2.2-Linux-i386.tar.gz -C /tmp/cmake --strip-components=1
 		else
@@ -16,6 +16,6 @@ case "$TRAVIS_OS_NAME" in
 	;;
 	osx)
 		wget http://www.cmake.org/files/v3.2/cmake-3.2.2-Darwin-x86_64.tar.gz
-		sudo tar -xf cmake-3.2.2-Darwin-x86_64.tar.gz -C /usr/local --strip-components=1
+		sudo tar -xf cmake-3.2.2-Darwin-x86_64.tar.gz -C /tmp/cmake --strip-components=1
 	;;
 esac

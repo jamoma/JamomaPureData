18105ba688b180c47808d6eef0787196a25f0068
Antoine Villeret <antoine.villeret+dev@gmail.com>
2015-06-03 00:12:21 +0200

    install GCC 4.9 as the default compiler (since on 12.04 4.6 is the default and not enough for Jamoma)

--- script/depinstall.sh
+++ script/depinstall.sh
@@ -6,6 +6,7 @@ case "$TRAVIS_OS_NAME" in
 		sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 		sudo apt-get -qq update
 		sudo apt-get -qq install gcc-4.9 g++-4.9
+		sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.9 60 --slave /usr/bin/g++ g++ /usr/bin/g++-4.9
 		if [ "x$ARCH" = "xrpi" ]; then
 			git clone -b Jamoma https://github.com/avilleret/tools.git
 		fi

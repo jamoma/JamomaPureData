62b79012ce7d84b262a911abbef64b5721417b44
Antoine Villeret <antoine.villeret+dev@gmail.com>
2015-06-02 23:53:58 +0200

    use default compiler (should work with GCC 4.8) and disable JamomaMax build (in case there are some artifacts in JamomaCore

--- script/build.sh
+++ script/build.sh
@@ -7,6 +7,6 @@ if [ "x$ARCH" = "xrpi" ]; then
 		/tmp/cmake/bin/cmake -DCMAKE_TOOLCHAIN_FILE=../Shared/CMake/toolchains/arm-linux-gnueabihf.cmake -DJAMOMAPD_INSTALL_FOLDER=${PWD}/RPi-bin -DCROSS_COMPILER_PATH=${PWD}/../tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/ .. 
 	fi		
 else
-	/tmp/cmake/bin/cmake .. -DCMAKE_CXX_COMPILER=g++-4.9 -DCMAKE_C_COMPILER=gcc-4.9
+	/tmp/cmake/bin/cmake .. -DBUILD_JAMOMAPD=ON -DBUILD_JAMOMAMAX=OFF
 fi
 make

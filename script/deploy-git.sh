c14252c347f8f4243a085332b31e9d5bfed2239c
Antoine Villeret <antoine.villeret@gmail.com>
2015-06-22 18:41:52 +0200

    try to fix build upload

--- script/deploy-git.sh
+++ script/deploy-git.sh
@@ -1,19 +1,21 @@
 #!/bin/sh
 
-ARCHIVE_NAME="${TRAVIS_BUILD_DIR}/JamomaPd-${TRAVIS_OS_NAME}_${ARCH}-${TRAVIS_TAG}.tgz"
-ssh -o StrictHostKeyChecking=no
-cd ${TRAVIS_BUILD_DIR}/build
-cmake -DCMAKE_INSTALL_COMPONENT=JamomaPd -P cmake_install.cmake
+if [ "x${TRAVIS_BRANCH}" == "xfeature/travis-build" ]; then
+  ARCHIVE_NAME="${TRAVIS_BUILD_DIR}/JamomaPd-${TRAVIS_OS_NAME}_${ARCH}-${TRAVIS_TAG}.tgz"
 
-cd ${TRAVIS_BUILD_DIR}/pd-package
-tar cvzf ${TRAVIS_BUILD_DIR}/${ARCHIVE_NAME} Jamoma/
+  cd ${TRAVIS_BUILD_DIR}/build
+  cmake -DCMAKE_INSTALL_COMPONENT=JamomaPd -P cmake_install.cmake
 
-if [ "$TRAVIS_BRANCH" == "feature/travis-build" ]; then
-    git checkout gh-pages
-    mv ${ARCHIVE_NAME} content/download/nightly-builds/
-    git add -f content/download/nightly-builds/${ARCHIVE_NAME}
-    git commit -m "Add built output"
-    git push https://$(GH_USER):$(GH_PASSWORD)@github.com/${TRAVIS_REPO_SLUG} gh-pages
+  cd ${TRAVIS_BUILD_DIR}/pd-package
+  tar cvzf "${TRAVIS_BUILD_DIR}/${ARCHIVE_NAME}" Jamoma/
+
+  cd ${TRAVIS_BUILD_DIR}
+
+  git checkout gh-pages
+  mv ${ARCHIVE_NAME} content/download/nightly-builds/
+  git add -f content/download/nightly-builds/${ARCHIVE_NAME}
+  git commit -m "Add built output"
+  git push https://${GH_USER}:${GH_PASSWORD}@github.com/${TRAVIS_REPO_SLUG} gh-pages
 fi
 
 return 0

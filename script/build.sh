39f9e4947465159a0393288e537deef057e73f8a
Antoine Villeret <antoine.villeret@gmail.com>
2015-06-24 01:43:32 +0200

    revert to a more boring but working script

--- script/build.sh
+++ script/build.sh
@@ -7,21 +7,21 @@ if [ "x${COVERITY_SCAN_BRANCH}" = "x1" ]; then
   exit 0
 fi
 
-CMAKE_OPTIONS="-DBUILD_JAMOMAPD=ON -DBUILD_JAMOMAMAX=OFF -DJAMOMAPD_INSTALL_FOLDER=${TRAVIS_BUILD_DIR}/pd-package"
-
-if [ "x$ARCH" = "xrpi" ]; then
-  CMAKE_OPTIONS="${CMAKE_OPTIONS} -DCMAKE_TOOLCHAIN_FILE=`readlink -f ../Shared/CMake/toolchains/arm-linux-gnueabihf.cmake` -DCROSS_COMPILER_PATH=`readlink -f ${PWD}/../tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/`"
-elif [ "x$ARCH" = "xmingw-64" ]; then
-  CMAKE_OPTIONS="${CMAKE_OPTIONS}  -DCMAKE_TOOLCHAIN_FILE=`readlink -f ../Shared/CMake/toolchains/mingw-64.cmake` -DJAMOMA_CORE_SRC_PATH=`readlink -f ${PWD}/../JamomaCore`"
-fi
-
-if [ "x$ARCH" = "xosx" ]; then
-  CMAKE_OPTIONS="${CMAKE_OPTIONS} -DFAT_BINARY=ON"
-fi
-
 mkdir -p build
 cd build
 
-/tmp/cmake/bin/cmake ${CMAKE_OPTIONS} .. && make -j 4
+# CMAKE_OPTIONS="-DBUILD_JAMOMAPD=ON -DBUILD_JAMOMAMAX=OFF -DJAMOMAPD_INSTALL_FOLDER=${TRAVIS_BUILD_DIR}/pd-package"
+
+if [ "x$ARCH" = "xrpi" ]; then
+  /tmp/cmake/bin/cmake -DBUILD_JAMOMAPD=ON -DBUILD_JAMOMAMAX=OFF -DJAMOMAPD_INSTALL_FOLDER=${TRAVIS_BUILD_DIR}/pd-package -DCMAKE_TOOLCHAIN_FILE=`readlink -f ../Shared/CMake/toolchains/arm-linux-gnueabihf.cmake` -DCROSS_COMPILER_PATH=`readlink -f ${PWD}/../tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/` ..
+elif [ "x$ARCH" = "xmingw-64" ]; then
+  /tmp/cmake/bin/cmake -DBUILD_JAMOMAPD=ON -DBUILD_JAMOMAMAX=OFF -DJAMOMAPD_INSTALL_FOLDER=${TRAVIS_BUILD_DIR}/pd-package -DCMAKE_TOOLCHAIN_FILE=`readlink -f ../Shared/CMake/toolchains/mingw-64.cmake` -DJAMOMA_CORE_SRC_PATH=`readlink -f ${PWD}/../JamomaCore` ..
+elif [ "x$ARCH" = "xosx" ]; then
+  /tmp/cmake/bin/cmake -DBUILD_JAMOMAPD=ON -DBUILD_JAMOMAMAX=OFF -DJAMOMAPD_INSTALL_FOLDER=${TRAVIS_BUILD_DIR}/pd-package -DFAT_BINARY=ON  ..
+else
+  /tmp/cmake/bin/cmake -DBUILD_JAMOMAPD=ON -DBUILD_JAMOMAMAX=OFF -DJAMOMAPD_INSTALL_FOLDER=${TRAVIS_BUILD_DIR}/pd-package ..
+fi
+
+make -j 4
 
 exit 0

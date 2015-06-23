52de4ae67f7c3a80a8102cea67979d562a1823f3
Antoine Villeret <antoine.villeret@gmail.com>
2015-06-23 11:37:53 +0200

    simplify build script since build matrix now takes care of arch/host

--- script/build.sh
+++ script/build.sh
@@ -6,14 +6,19 @@ if [ "x${COVERITY_SCAN_BRANCH}" = "x1" ]; then
   error "looks like we are running a coverity-scan build: stopping"
   exit 0
 fi
+
+CMAKE_OPTIONS="-DBUILD_JAMOMAPD=ON -DBUILD_JAMOMAMAX=OFF -DJAMOMAPD_INSTALL_FOLDER=${TRAVIS_BUILD_DIR}/pd-package"
+if [ "x$ARCH" = "xrpi" ]; then
+  CMAKE_OPTIONS+=" -DCMAKE_TOOLCHAIN_FILE=../Shared/CMake/toolchains/arm-linux-gnueabihf.cmake -DCROSS_COMPILER_PATH=${PWD}/../tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/"
+fi
+
+if [ "x$ARCH" = "xosx" ]; then
+  CMAKE_OPTIONS+=" -DFAT_BINARY=ON"
+fi
+
 mkdir -p build
 cd build
-if [ "x$ARCH" = "xrpi" ]; then
-	if [ "$TRAVIS_OS_NAME" != "osx" ]; then
-		/tmp/cmake/bin/cmake -DBUILD_JAMOMAPD=ON -DBUILD_JAMOMAMAX=OFF -DJAMOMAPD_INSTALL_FOLDER=${TRAVIS_BUILD_DIR}/pd-package -DCMAKE_TOOLCHAIN_FILE=../Shared/CMake/toolchains/arm-linux-gnueabihf.cmake -DJAMOMAPD_INSTALL_FOLDER=${PWD}/RPi-bin -DCROSS_COMPILER_PATH=${PWD}/../tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/ ..
-		make
-	fi
-else
-	/tmp/cmake/bin/cmake -DBUILD_JAMOMAPD=ON -DBUILD_JAMOMAMAX=OFF -DJAMOMAPD_INSTALL_FOLDER=${TRAVIS_BUILD_DIR}/pd-package ..
-	make
-fi
+
+/tmp/cmake/bin/cmake ${CMAKE_OPTIONS} .. && make -j 4
+
+exit 0

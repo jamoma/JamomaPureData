42a3030c34ea0e85a319392e818b8d31a262c26a
Antoine Villeret <antoine.villeret@gmail.com>
2015-07-02 20:37:21 +0200

    export symbol from JamomaPd.dll

--- Shared/CMake/toolchains/mingw-64.cmake
+++ Shared/CMake/toolchains/mingw-64.cmake
@@ -6,8 +6,8 @@
 SET(CMAKE_SYSTEM_NAME Windows)
 
 # which compilers to use for C and C++
-SET(CMAKE_C_COMPILER i686-w64-mingw32-gcc)
-SET(CMAKE_CXX_COMPILER i686-w64-mingw32-g++)
+SET(CMAKE_C_COMPILER i686-w64-mingw32-gcc-posix)
+SET(CMAKE_CXX_COMPILER i686-w64-mingw32-g++-posix)
 SET(CMAKE_RC_COMPILER i686-w64-mingw32-windres)
 
 # here is the target environment located
@@ -22,3 +22,7 @@ SET(CMAKE_FIND_ROOT_PATH /usr/i686-w64-mingw32/
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
+add_definitions(-DTT_PLATFORM_WIN)
+add_definitions(-DTTSTATIC)
+

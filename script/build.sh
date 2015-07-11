025fedd6612efc9d86406dd9d3a1bbef91387aab
Antoine Villeret <antoine.villeret@gmail.com>
2015-07-11 12:14:36 +0200

    simplify build script

--- script/build.sh
+++ script/build.sh
@@ -19,11 +19,6 @@ cd build
 
 # CMAKE_OPTIONS="-DBUILD_JAMOMAPD=ON -DBUILD_JAMOMAMAX=OFF -DJAMOMAPD_INSTALL_FOLDER=${TRAVIS_BUILD_DIR}/pd-package"
 
-if [ "x$TRAVIS_OS_NAME" = "xlinux" -a "x$ARCH" != "xmingw-w64" ]; then
-  export CC=gcc-4.9
-  export CXX=g++-4.9
-fi
-
 if [ "x$ARCH" = "xrpi" ]; then
   /tmp/cmake/bin/cmake -DPD_MAIN_PATH=`readlink -f ${PWD}/../pd` -DBUILD_JAMOMAPD=ON -DBUILD_JAMOMAMAX=OFF -DJAMOMAPD_INSTALL_FOLDER=${TRAVIS_BUILD_DIR}/pd-package -DCMAKE_TOOLCHAIN_FILE=`readlink -f ../Shared/CMake/toolchains/arm-linux-gnueabihf.cmake` -DCROSS_COMPILER_PATH=`readlink -f ${PWD}/../tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/` ..
 elif [ "x$ARCH" = "xmingw-w64" ]; then
@@ -31,6 +26,8 @@ elif [ "x$ARCH" = "xmingw-w64" ]; then
 elif [ "x$TRAVIS_OS_NAME" = "xosx" ]; then
   /tmp/cmake/bin/cmake -DPD_MAIN_PATH=`greadlink -f ${PWD}/../pd` -DBUILD_JAMOMAPD=ON -DBUILD_JAMOMAMAX=OFF -DJAMOMAPD_INSTALL_FOLDER=${TRAVIS_BUILD_DIR}/pd-package -DFAT_BINARY=ON  ..
 else
+  export CC=gcc-4.9
+  export CXX=g++-4.9
   /tmp/cmake/bin/cmake -DPD_MAIN_PATH=`readlink -f ${PWD}/../pd` -DBUILD_JAMOMAPD=ON -DBUILD_JAMOMAMAX=OFF -DJAMOMAPD_INSTALL_FOLDER=${TRAVIS_BUILD_DIR}/pd-package ..
 fi
 

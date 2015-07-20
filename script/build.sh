be7f0865c8fe6e8808f5798a58e2c5278d11083d
Antoine Villeret <antoine.villeret@gmail.com>
2015-07-20 12:46:13 +0200

    simplify CMake options

--- script/build.sh
+++ script/build.sh
@@ -17,18 +17,23 @@ fi
 mkdir -p build
 cd build
 
-# CMAKE_OPTIONS="-DBUILD_JAMOMAPD=ON -DBUILD_JAMOMAMAX=OFF -DJAMOMAPD_INSTALL_FOLDER=${TRAVIS_BUILD_DIR}/pd-package"
+# common options to all jobs
+CMAKE_OPTIONS="-DBUILD_JAMOMAPD=ON -DBUILD_JAMOMAMAX=OFF -DJAMOMAPD_INSTALL_FOLDER=${TRAVIS_BUILD_DIR}/pd-package"
 
 if [ "x$ARCH" = "xrpi" ]; then
-  /tmp/cmake/bin/cmake -DPD_MAIN_PATH=`readlink -f ${PWD}/../pd` -DBUILD_JAMOMAPD=ON -DBUILD_JAMOMAMAX=OFF -DJAMOMAPD_INSTALL_FOLDER=${TRAVIS_BUILD_DIR}/pd-package -DCMAKE_TOOLCHAIN_FILE=`readlink -f ../Shared/CMake/toolchains/arm-linux-gnueabihf.cmake` -DCROSS_COMPILER_PATH=`readlink -f ${PWD}/../tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/` ..
+  CMAKE_OPTIONS="$CMAKE_OPTIONS -DCMAKE_TOOLCHAIN_FILE=`readlink -f ../Shared/CMake/toolchains/arm-linux-gnueabihf.cmake` -DCROSS_COMPILER_PATH=`readlink -f ${PWD}/../tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/`"
 elif [ "x$ARCH" = "xmingw-w64" ]; then
-  /tmp/cmake/bin/cmake -DPD_MAIN_PATH=`readlink -f ${PWD}/../pd` -DBUILD_JAMOMAPD=ON -DBUILD_JAMOMAMAX=OFF -DJAMOMAPD_INSTALL_FOLDER=${TRAVIS_BUILD_DIR}/pd-package -DCMAKE_TOOLCHAIN_FILE=`readlink -f ../Shared/CMake/toolchains/mingw-64.cmake` -DJAMOMA_CORE_SRC_PATH=`readlink -f ${PWD}/../JamomaCore` ..
+  CMAKE_OPTIONS="$CMAKE_OPTIONS -DCMAKE_TOOLCHAIN_FILE=`readlink -f ../Shared/CMake/toolchains/mingw-64.cmake` -DJAMOMA_CORE_SRC_PATH=`readlink -f ${PWD}/../JamomaCore`"
 elif [ "x$TRAVIS_OS_NAME" = "xosx" ]; then
-  /tmp/cmake/bin/cmake -DPD_MAIN_PATH=`greadlink -f ${PWD}/../pd` -DBUILD_JAMOMAPD=ON -DBUILD_JAMOMAMAX=OFF -DJAMOMAPD_INSTALL_FOLDER=${TRAVIS_BUILD_DIR}/pd-package -DFAT_BINARY=ON  ..
+  CMAKE_OPTIONS="$CMAKE_OPTIONS -DPD_MAIN_PATH=`greadlink -f ${PWD}/../pd` -DFAT_BINARY=ON"
 else
   export CC=gcc-4.9
   export CXX=g++-4.9
-  /tmp/cmake/bin/cmake -DPD_MAIN_PATH=`readlink -f ${PWD}/../pd` -DBUILD_JAMOMAPD=ON -DBUILD_JAMOMAMAX=OFF -DJAMOMAPD_INSTALL_FOLDER=${TRAVIS_BUILD_DIR}/pd-package ..
 fi
 
+if [ "x$TRAVIS_OS_NAME" = "xlinux" ]; then
+  CMAKE_OPTIONS="$CMAKE_OPTIONS -DPD_MAIN_PATH=`readlink -f ${PWD}/../pd`"
+fi
+
+/tmp/cmake/bin/cmake .. "$CMAKE_OPTIONS"
 make -j 4

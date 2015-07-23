bb9e81c0daee3b1e56ad00638299e9be3aa57833
Antoine Villeret <antoine.villeret@gmail.com>
2015-07-24 01:30:16 +0200

    fix mistake
    grrrr

--- script/build-mingw-w64.sh
+++ script/build-mingw-w64.sh
@@ -166,7 +166,7 @@ echo "***************"
 
 mkdir -p mingw-crt-build
 cd mingw-crt-build
-./mingw-w64-src/mingw-w64-crt/configure \
+../mingw-w64-src/mingw-w64-crt/configure \
     CPPFLAGS=-I"$MINGW/x86_64-w64-mingw32/include/" \
     --prefix=$MINGW/x86_64-w64-mingw32 \
     --with-sysroot=$MINGW \

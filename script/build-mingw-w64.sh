933a5a35afb324a4b942160a95d38d062204809d
Antoine Villeret <antoine.villeret@gmail.com>
2015-07-24 01:14:00 +0200

    the order may matter

--- script/build-mingw-w64.sh
+++ script/build-mingw-w64.sh
@@ -166,7 +166,8 @@ echo "***************"
 
 mkdir -p mingw-crt-build
 cd mingw-crt-build
-CPPFLAGS=-I"$MINGW/x86_64-w64-mingw32/include/" ../mingw-w64-src/mingw-w64-crt/configure \
+./mingw-w64-src/mingw-w64-crt/configure \
+    CPPFLAGS=-I"$MINGW/x86_64-w64-mingw32/include/" \
     --prefix=$MINGW/x86_64-w64-mingw32 \
     --with-sysroot=$MINGW \
     --enable-lib32 \

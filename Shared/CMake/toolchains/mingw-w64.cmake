6c0f28d3589a05670a7764a11e957ae86652c772
Antoine Villeret - Ubuntu 12.04 server <antoine.villeret@gmail.com>
2015-07-25 23:08:10 +0200

    fix libxml2-2.dll path

--- Shared/CMake/toolchains/mingw-w64.cmake
+++ Shared/CMake/toolchains/mingw-w64.cmake
@@ -27,5 +27,5 @@ add_definitions(-DTTSTATIC)
 install(FILES
 ${CROSS_COMPILER_PATH}/x86_64-w64-mingw32/lib32/libstdc++-6.dll
 ${CROSS_COMPILER_PATH}/x86_64-w64-mingw32/lib32/libgcc_s_sjlj-1.dll
-              ${JAMOMA_CORE_SRC_PATH}/Foundation/library/libxml2/win32/libmingw-w64-32bit/bin/libxml2-2.dll
+${CROSS_COMPILER_PATH}/local32/bin/libxml2-2.dll
         DESTINATION ${JAMOMAPD_INSTALL_FOLDER}/Jamoma)

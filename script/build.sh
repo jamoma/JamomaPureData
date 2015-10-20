0953f4343df8a85ec8bc16ced44c53387d9ada47
Antoine Villeret <antoine.villeret@gmail.com>
2015-10-20 16:58:15 +0200

    force to use clang-3.6 instead of 3.4

--- script/build.sh
+++ script/build.sh
@@ -39,7 +39,7 @@ else
 fi
 
 if [  "x${COMPILER}" = "xclang" ]; then
-  CMAKE_OPTIONS="$CMAKE_OPTIONS -DCMAKE_CXX_COMPILER=clang++"
+  CMAKE_OPTIONS="$CMAKE_OPTIONS -DCMAKE_CXX_COMPILER=clang++-3.6"
 fi
 
 echo "Configuring with CMAKE_OPTIONS=${CMAKE_OPTIONS}"
